75dc92c69dd82de25a30337b3b4253e0c8b93534
tiger <tiger.12.ht@gmail.com>
2025-12-14 08:50:54 -0800

    initial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { ScrollTrigger, SplitText } from "gsap/all"
+
+gsap.registerPlugin( ScrollTrigger, SplitText );
+
+const App = () => {
+
+
+  return (
+    <div>
+      App
+    </div>
+  )
+}
+
+export default App
